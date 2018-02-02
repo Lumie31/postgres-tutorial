f5d886eb4dbfc9206915a9e017d9b139b65d0273
unknown <olumideokedusi@gmail.com>
2018-02-02 13:58:57 +0100

    new branch test

--- test/test.js
+++ test/test.js
@@ -1,7 +1,12 @@
 var assert = require('chai').assert;
 
 describe('A basic test', function () {
-  it('should pass when everything is okay', function () {
-    assert('foo' !== 'bar', 'foo is not bar');
+    it('should pass when everything is okay', function () {
+      assert('foo' !== 'bar', 'foo is not bar');
+    })
+  },
+
+  it('should fail when everything isn\'t okay', function () {
+    assert(Array.isArray([]), 'empty arrays are arrays');
   })
-})
+)
